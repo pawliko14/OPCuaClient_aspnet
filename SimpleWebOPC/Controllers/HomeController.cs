9df820cd8dfce508630e8d20bf5b31dace829a20
Konrad Paw <pawliko14@gmail.com>
2020-05-26 11:11:59 +0200

    New functionalities

--- SimpleWebOPC/Controllers/HomeController.cs
+++ SimpleWebOPC/Controllers/HomeController.cs
@@ -47,7 +47,6 @@ namespace SimpleWebOPC.Controllers
             ViewBag.Message = "wartosc ->  IB0: " + IB0;
 
             int result = int.Parse(IB0);
-
             int inv = 0;
 
 
@@ -59,7 +58,7 @@ namespace SimpleWebOPC.Controllers
 
         }
 
-        [HttpPost]
+        [HttpPost] // auto
         public string DB11_DBB6_0()
         {
             string IB0 = null;
@@ -68,7 +67,170 @@ namespace SimpleWebOPC.Controllers
         }
 
 
+        [HttpPost] //mda
+        public string DB11_DBB6_1()
+        {
+            string val = null;
+            val = Read_DB11_DBB6_1();
+            return val;
+        }
 
+        [HttpPost] //JOG
+        public string DB11_DBB6_2()
+        {
+            string val = null;
+            val = Read_DB11_DBB6_2();
+            return val;
+        }
+
+        [HttpPost] //Ref point
+        public string DB11_DBB7_2()
+        {
+            string val = null;
+            val = Read_DB11_DBB7_2();
+            return val;
+        }
+
+        [HttpPost] // calculate work mode
+        public string Work_mode()
+        {
+            string mode = null;
+            string Auto = Read_DB11_DBB6_0();
+            string MDA = Read_DB11_DBB6_1();
+            string Jog = Read_DB11_DBB6_2();
+            string refpoint = Read_DB11_DBB7_2(); 
+
+            if(Auto.Equals("True") && Jog.Equals("False"))
+            {
+                mode = "Auto Mode";
+            }
+            else if( MDA.Equals("True") && Jog.Equals("False"))
+            {
+                mode = "MDA mode";
+            }
+            else if(Jog.Equals("True") && refpoint.Equals("False"))
+            {
+                mode = "Jog mode";
+            }
+            else if(Jog.Equals("True") && refpoint.Equals("True") )
+            {
+                mode = "ref & jog mode";
+            }
+            else
+            {
+                mode = "unidentify mode";
+            }
+            return mode;
+        }
+
+        [HttpPost] //calculate key position
+        public string Key_pos()
+        {
+            string final_pos = null;
+            string pos1 = Read_DB10_DBX56_5();
+            string pos2 = Read_DB10_DBX56_6();
+            string pos3 = Read_DB10_DBX56_7();
+
+            if(pos1.Equals("True") && pos2.Equals("False") && pos3.Equals("False"))
+            {
+                final_pos = "Position 1";
+            }
+            else if (pos1.Equals("False") && pos2.Equals("True") && pos3.Equals("False"))
+            {
+                final_pos = "Position 2";
+                 
+            }
+            else if (pos1.Equals("False") && pos2.Equals("False") && pos3.Equals("True"))
+            {
+                final_pos = "Position 3";
+            }
+            else
+            {
+                final_pos = " unidentify position";
+            }
+            return final_pos;
+        }
+
+      
+
+        [HttpPost] //Prog running
+        public string DB20_DBX35_0()
+        {
+            string val = null;
+            val = Read_DB20_DBX35_0();
+            return val;
+        }
+
+        [HttpPost] //Prog running 2
+        public string Q204_0()
+        {
+            string val = null;
+            val = Read_Q204_0();
+            return val;
+        }
+
+        [HttpPost] //Prog in idle
+        public string Q204_1()
+        {
+            string val = null;
+            val = Read_Q204_1();
+            return val;
+        }
+
+        [HttpPost] //Alarm is active
+        public string Q204_2()
+        {
+            string val = null;
+            val = Read_Q204_2();
+            return val;
+        }
+
+        [HttpPost] //feed start
+        public string Q1_7()
+        {
+            string val = null;
+            val = Feed_start();
+            return val;
+        }
+
+        [HttpPost] //feed stop
+        public string Q1_6()
+        {
+            string val = null;
+            val = Feed_stop();
+            return val;
+        }
+
+        [HttpPost] //Spindle start
+        public string Q2_1()
+        {
+            string val = null;
+            val = Spindle_start();
+            return val;
+        }
+
+        [HttpPost] //Spindle stop
+        public string Q2_0()
+        {
+            string val = null;
+            val = Spindle_stop();
+            return val;
+        }
+
+        [HttpPost] // List of axis movements
+        public List<string> Axis_movements()
+        {
+            List<string> axis_list = new List<string>();
+            axis_list = Axis_movement();
+
+            return axis_list;
+        }
+
+
+        /*
+         * 
+         * 
+         */
 
         private string Read_IB3()
         {
@@ -177,6 +339,8 @@ namespace SimpleWebOPC.Controllers
             return results[0];
         }
 
+        
+
         //PROG RUNNING
         private string Read_DB20_DBX35_0()
         {
@@ -283,6 +447,34 @@ namespace SimpleWebOPC.Controllers
 
 
             return results[0];
+        }
+
+        //Axis movement
+        private List<string> Axis_movement()
+        {
+            List<string> axis_results = new List<string>();
+            List<string> nodesToRead = new List<string>();
+
+            nodesToRead.Add(new NodeId("/Plc/DB31.DBX64.6", 2).ToString()); // x axis minus
+            nodesToRead.Add(new NodeId("/Plc/DB31.DBX64.7", 2).ToString()); // x axis plus
+
+            nodesToRead.Add(new NodeId("/Plc/DB32.DBX64.6", 2).ToString()); // y axis minux
+            nodesToRead.Add(new NodeId("/Plc/DB32.DBX64.7", 2).ToString()); // y axis plus
+
+            nodesToRead.Add(new NodeId("/Plc/DB33.DBX64.6", 2).ToString()); // z axis minus
+            nodesToRead.Add(new NodeId("/Plc/DB33.DBX64.7", 2).ToString()); // z axis plus
+
+            nodesToRead.Add(new NodeId("/Plc/DB34.DBX64.6", 2).ToString()); // w axis minus
+            nodesToRead.Add(new NodeId("/Plc/DB34.DBX64.7", 2).ToString()); // w axis plus
+
+            nodesToRead.Add(new NodeId("/Plc/DB36.DBX64.6", 2).ToString()); // s axis minus
+            nodesToRead.Add(new NodeId("/Plc/DB36.DBX64.7", 2).ToString()); // s axis plus
+
+
+            axis_results = OpcUastartup.get_m_server().ReadValues(nodesToRead);
+
+
+            return axis_results;
         }
     }
 }
